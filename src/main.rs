1340d768fc73fe8ac2cadfa8906f3b4da12b7f52
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2017-05-25 17:30:00 +0400

    Register user

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 #[macro_use] extern crate nickel;
+#[macro_use] extern crate mysql;
 extern crate mustache;
 extern crate rustc_serialize;
-extern crate mysql;
 
 use std::path::Path;
 use std::collections::HashMap;
 use mustache::{Data,MapBuilder};
-use nickel::{Nickel, HttpRouter, StaticFilesHandler, Mount, Request, Response, MiddlewareResult};
+use nickel::{Nickel, HttpRouter, StaticFilesHandler, Mount, Request, Response, MiddlewareResult,FormBody};
 
-use mysql::{Pool,error};
+use mysql::{Pool,QueryResult};
+use mysql::error::{Error,MySqlError};
 
 #[derive(Debug, PartialEq, Eq)]
 struct User{
@@ -36,7 +37,7 @@ impl AppConfig{
 
 fn main() {
     let config = AppConfig{
-        db_string: "mysql://root:password@localhost:3307/surveys".to_owned(),
+        db_string: "mysql://root:password@localhost:3306/surveys".to_owned(),
         host: "127.0.0.1".to_owned(),
         port: "8080".to_owned(),
     };
@@ -76,9 +77,43 @@ fn register<'a>(req: &mut Request<AppConfig>, mut res: Response<'a,AppConfig>) -
         }
     };
 
+    let params = req.form_body().unwrap();
+    let email = params.get("email").unwrap();
+    let password = params.get("password").unwrap();
+    let first_name = params.get("first_name").unwrap();
+    let last_name = params.get("last_name").unwrap();
+
+    let mut stmt = pool.prepare(r"INSERT INTO users (username,password,first_name,last_name,email,is_active,is_staff)VALUES(:username,:password,:first_name,:last_name,:email,:is_active,:is_staff)").unwrap();
+    let qr = match stmt.execute(params!{
+        "username" => email,
+        "password" => password,
+        "first_name" => first_name,
+        "last_name" => last_name,
+        "email" => email,
+        "is_active" => 1,
+        "is_staff" => 0,
+    }) {
+        Ok(qr) => qr,
+        Err(err) => {
+            let message = match(err){
+                Error::MySqlError(my_err) => if my_err.code == 1062 { "Username already exists".to_owned() }else{ format!("{}",my_err) },
+                _ => format!("{}",err),
+            };
+
+            let data = ViewModel{
+                has_error: true,
+                error: message,
+            };
+            return res.render("templates/index.tpl",&data);
+        }
+    };
+    
+    let message = if qr.affected_rows() == 1 { "Sign-up successful".to_owned() }else{ "WTF".to_owned() };
+
     let data = ViewModel {
         has_error: true,
-        error: "Database connected successfully".to_owned(),
+        error: message,
     };
+    
     return res.render("templates/index.tpl", &data);    
 }
